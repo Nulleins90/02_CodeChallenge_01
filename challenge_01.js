04ae007fc8728863e6175c8faf0cac85b71556f2
Christian <chris.moeller-bosse@gmx.de>
2021-11-18 16:54:09 +0100

    Hausaufgabe

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,16 @@ Berechnen Sie die Summe der beiden Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+
+// Zahlen werden angefordet
+
+let number1 = Number(prompt("Erste Zahl eingeben:"));
+let number2 = Number(prompt("Zweite Zahl eingeben:"));
+
+// Berechnung
+
+// let sum = (number1 + number2);
+
+// Ausgabe
+
+console.log(number1 + " + " + number2 + " = " + (number1 + number2));
